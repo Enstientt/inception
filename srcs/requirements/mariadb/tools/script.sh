34c3a7e0da3bd74b9727fc41e65bf93094b7b5e2
zessadqu <zikoessad@gmail.com>
2023-08-29 15:36:12 +0100

    ds

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Define the database name, user, and password
+# Create SQL commands using the variables
+echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+
+# Run SQL commands using the mysql command
+mysql < db1.sql
+
