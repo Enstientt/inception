124cb2d2ee03de39d0fa09896aa6865287828e87
zessadqu <zikoessad@gmail.com>
2023-09-01 21:36:48 +0100

    ds

--- makefile
+++ makefile
@@ -1,20 +1,20 @@
 all:
-	docker compose -f srcs/docker-compose.yaml build
+	docker-compose -f srcs/docker-compose.yaml build
 	 
 
 build: all
 
 up: 
-	docker compose -f srcs/docker-compose.yaml up -d
+	docker-compose -f srcs/docker-compose.yaml up -d
 
 down: 
-	docker compose -f srcs/docker-compose.yaml down
+	docker-compose -f srcs/docker-compose.yaml down
 
 start: 
-	docker compose -f srcs/docker-compose.yaml start
+	docker-compose -f srcs/docker-compose.yaml start
 
 stop: 
-	docker compose -f srcs/docker-compose.yaml stop
+	docker-compose -f srcs/docker-compose.yaml stop
 
 restart: 
-	docker compose -f srcs/docker-compose.yaml restart
+	docker-compose -f srcs/docker-compose.yaml restart
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-# Define the database name, user, and password
-# Create SQL commands using the variables
-echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+
+service mariadb start 
+sleep 10
+
+
+echo "CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $MARIADB_NAME.* TO '$MARIADB_USER'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 # Run SQL commands using the mysql command
-mysql < db1.sql
+mariadb < db1.sql
+
+sed -i "s/127.0.0.1/0.0.0.0/g"  /etc/mysql/mariadb.conf.d/50-server.cnf
+
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
 
