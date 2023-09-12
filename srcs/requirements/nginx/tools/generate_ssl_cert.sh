1aa6cea32a2f6dc5a0f9c8b7f03f4367c7bf78ac
Zakariae Essadqui <zessadqu@e3r2p16.1337.ma>
2023-09-12 22:39:17 +0100

    ds

--- srcs/requirements/nginx/tools/generate_ssl_cert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p /etc/nginx/ssl
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/CN=localhost" # Replace with your domain or IP
-
-exec "$@"
